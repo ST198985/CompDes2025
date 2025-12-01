c8b844781e113748b322d41703022004ef653402
ST198985 <st198985@stud.uni-stuttgart.de>
2025-12-02 00:50:47 +0100

    Create MA 7_Use of AI.py

---
+++ Mini-assignments/MA_07/MA 7_Use of AI.py
@@ -0,0 +1,42 @@
+"""
+Questionnaire: For what purposes did you use Generative AI / LLMs when completing your assignment?
+[] Not at all
+[] Which ones did you use? (e.g., ChatGPT, Bard, etc.) _Perplexity_
+[] Explaining programming concepts
+[] Practicing coding exercises
+[] Debugging code
+[] Reviewing your Python code
+[] Optimizing code
+[] Writing or completing code
+[] Other (please specify): _____________
+
+"""  
+
+"""
+Assignment due 02/12/2025
+
+Let’s assume that S is a set of 3d points of a defined spacing in the workspace of the robot.
+
+Using the Virtual Robot plugin in Grasshopper:
+
+In a 3D grid of points
+
+Define the subset S1 of points in S that is reachable by the robot.
+
+Define the subset S2 of points in S where the robot does not collide with itself using axis angles.
+
+Define the subset S3 of points in S that meets the criteria of both S1 and S2, with any additional criteria you see fit.
+
+Define groups and conceptualize idea for Light Sculpture. Prepare initial geometry as a functional robot path for desk crit next week.
+
+Parameters:
+Point spacing
+Defined axis ranges (min to max)
+Modify the definition below or use the definition that we started in class.
+
+Deliverables:
+
+Submission (of GH file) on Github, optionally include a link to your pull request in a Text file as part of your submisson on ILIAS
+Upload on ILIAS
+Be able to discuss you solution in class
+"""
